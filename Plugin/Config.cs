dd9f098fec5d72aecb9a21f2dd42cc7f21374081
ficusmods <ficusmods@users.noreply.github.com>
2023-10-27 21:14:36 +0200

    U12 update

--- Plugin/Config.cs
+++ Plugin/Config.cs
@@ -5,11 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Text.RegularExpressions;
+using ThunderRoad;
 
 namespace Scavenger
 {
     public class Config
     {
+        [ModOption(name = "Enable item tracking", defaultValueIndex = 1)]
+        public static bool TrackingEnabled = true;
+        [ModOption(name = "Disable particles")]
         public static bool ItemSpotNoParticles = false;
         public static int TrackedItemCount = 50;
         public static float ItemSpotSpawnHeight = 0.1f;
@@ -24,7 +28,7 @@ namespace Scavenger
         public static float[] ShineColorOOR = new float[3] { 1.0f, 1.0f, 1.0f }; // out of range
         public static float[] ShineColorIR = new float[3] { 0.35f, 0.95f, 0.1f }; // in range
 
-        public static bool ItemExclusionListUseRegex = false;
+        public static bool ItemExclusionListUseRegex = true;
         public static HashSet<string> ItemExclusionList = new HashSet<string>();
         public static List<Regex> ItemExclusionListRegex = new List<Regex>();
     }
--- Plugin/LoadModule.cs
+++ Plugin/LoadModule.cs
@@ -4,97 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 using ThunderRoad;
 using UnityEngine;
-
+using Newtonsoft.Json;
 using System.Text.RegularExpressions;
 
 namespace Scavenger
 {
-    public class LoadModule : LevelModule
+    public class LoadModule : ThunderScript
     {
 
-        public string mod_version = "0.0";
-        public string mod_name = "UnnamedMod";
+        public string mod_version = "1.4";
+        public string mod_name = "Scavenger";
         public string logger_level = "Basic";
 
-        public bool ItemSpotNoParticles
-        {
-            get => Config.ItemSpotNoParticles;
-            set => Config.ItemSpotNoParticles = value;
-        }
-        public int TrackedItemCount
-        {
-            get => Config.TrackedItemCount;
-            set => Config.TrackedItemCount = value >= 0 ? value : 0;
-        }
-        public float ItemSpotSpawnHeight
-        {
-            get => Config.ItemSpotSpawnHeight;
-            set => Config.ItemSpotSpawnHeight = value >= 0 ? value : 0;
-        }
-        public float ItemSpotLabelVisibleDistance
-        {
-            get => Config.ItemSpotLabelVisibleDistance;
-            set => Config.ItemSpotLabelVisibleDistance = value >= 0 ? value : 0;
-        }
-        public float ItemSpotShineVisibleDistance
-        {
-            get => Config.ItemSpotShineVisibleDistance;
-            set => Config.ItemSpotShineVisibleDistance = value >= 0 ? value : 0;
-        }
-        public float ItemSpotGrabDistance
-        {
-            get => Config.ItemSpotGrabDistance;
-            set => Config.ItemSpotGrabDistance = value >= 0 ? value : 0;
-        }
-        public float ItemSpotShineScale
-        {
-            get => Config.ItemSpotShineScale;
-            set => Config.ItemSpotShineScale = value >= 0 ? value : 0;
-        }
-        public float ItemSpotLabelScale
-        {
-            get => Config.ItemSpotLabelScale;
-            set => Config.ItemSpotLabelScale = value >= 0 ? value : 0;
-        }
-        public float ItemSpotLabelHeight
-        {
-            get => Config.ItemSpotLabelHeight;
-            set => Config.ItemSpotLabelHeight = value >= 0 ? value : 0;
-        }
-        public float ItemDropRaycastSphereSize
-        {
-            get => Config.ItemDropRaycastSphereSize;
-            set => Config.ItemDropRaycastSphereSize = value >= 0 ? value : 0;
-        }
-        public float ItemDropRaycastLength
-        {
-            get => Config.ItemDropRaycastLength;
-            set => Config.ItemDropRaycastLength = value >= 0 ? value : 0;
-        }
+        public List<String> ItemExclusionList;
 
-        public bool ItemExclusionListUseRegex
+        public override void ScriptEnable()
         {
-            get => Config.ItemExclusionListUseRegex;
-            set => Config.ItemExclusionListUseRegex = value;
-        }
-
-        public IList<String> ItemExclusionList;
-        public float[] ShineColorOOR;
-        public float[] ShineColorIR;
-
-        public override IEnumerator OnLoadCoroutine()
-        {
+            base.ScriptEnable();
             Logger.init(mod_name, mod_version, logger_level);
 
             Logger.Basic("Loading " + mod_name);
+            LoadItemExclusionList();
             CompileItemExclusionList();
-            Config.ShineColorOOR[0] = ShineColorOOR[0]; Config.ShineColorOOR[1] = ShineColorOOR[1]; Config.ShineColorOOR[2] = ShineColorOOR[2];
-            Config.ShineColorIR[0] = ShineColorIR[0]; Config.ShineColorIR[1] = ShineColorIR[1]; Config.ShineColorIR[2] = ShineColorIR[2];
             EventManager.onLevelLoad += EventManager_onLevelLoad;
-            return base.OnLoadCoroutine();
+        }
+
+        private void LoadItemExclusionList()
+        {
+            ItemExclusionList = JsonConvert.DeserializeObject<List<String>>(File.ReadAllText(
+                FileManager.GetFullPath(FileManager.Type.JSONCatalog, FileManager.Source.Mods, "Scavenger/exclude.json")
+                ));
         }
 
         private void CompileItemExclusionList()
